f7acedb3d0a56958e000a1f496ac70f44986d0d2
sai-uday1 <161728136+sai-uday1@users.noreply.github.com>
2024-03-22 17:33:42 +0530

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("editor-options")
+require("lazy").setup("plugins")
+
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
